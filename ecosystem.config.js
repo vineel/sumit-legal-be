a05b8876d117afc0401c7dab99cf46ccff6e48d4
Vineel Shah <vineel@vineel.com>
2025-10-16 23:09:07 -0400

    trying to fix ecosystem to run well in pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,24 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+const projectRoot = __dirname;
+const envPath = path.join(projectRoot, '.env');
+const envConfig = dotenv.config({ path: envPath }).parsed || {};
+
+const env = {
+  ...envConfig,
+  NODE_ENV: envConfig.NODE_ENV || 'production',
+  PORT: envConfig.PORT || 5001,
+};
+
 module.exports = {
-  apps: [{
-    name: "sumit-be",
-    cwd: "/home/vineel/sumit-be",
-    script: "npm",
-    args: "start",
-    env: require("dotenv").config().parsed
-  }]
-}
+  apps: [
+    {
+      name: 'sumit-legal-be',
+      cwd: projectRoot,
+      script: 'server.js',
+      interpreter: 'node',
+      env,
+    },
+  ],
+};
